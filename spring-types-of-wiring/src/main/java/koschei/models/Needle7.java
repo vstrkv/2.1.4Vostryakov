b04a24a7af915e4316e630634b5930ab652c4851
vstrkv <37896140+vstrkv@users.noreply.github.com>
2021-05-12 01:39:46 +0700

    Update Needle7.java

--- spring-types-of-wiring/src/main/java/koschei/models/Needle7.java
+++ spring-types-of-wiring/src/main/java/koschei/models/Needle7.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
 
-    private final Deth8 deth;
+    private Deth8 deth;
 
     @Autowired
-    public Needle7 (Deth8 deth) { this.deth = deth; }
+    public void setDeth(Deth8 deth) {
+        this.deth = deth;
+    }
+
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + deth.toString();
